28246473d80d7a3fbd290a950329696c0a3fb570
foxytouxxx <143593532+foxytouxxx@users.noreply.github.com>
2023-12-10 13:33:03 +0100

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -1,3 +1,9 @@
+# A Script to auto install Pufferpanel 
+
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 sudo apt-get install pufferpanel
 sudo systemctl enable pufferpanel
+sudo pufferpanel user add --email foxytoux@gmail.com --name foxytoux --password Fox21200 --admin
+sudo systemctl enable --now pufferpanel
+
+# I uses https://docs.pufferpanel.com/en/2.x/installing.html to make this easy script, please look over there is there is any problem
